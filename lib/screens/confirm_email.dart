dca16c3eb47f3b5611078e533d833e2cf4eee796
Oluwasomidotun <victoroluwasomidotun@gmail.com>
2024-10-22 01:39:58 +0100

    refactoring and edit ci file

---
+++ lib/screens/confirm_email.dart
@@ -0,0 +1,168 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:kings_store/Components/colors.dart';
+import 'package:kings_store/screens/signin.dart';
+import 'package:kings_store/screens/signup.dart';
+import 'package:kings_store/utils/buttons.dart';
+
+class ConfirmEmailPage extends StatefulWidget {
+  const ConfirmEmailPage({super.key});
+
+  @override
+  State<ConfirmEmailPage> createState() => _ConfirmEmailPageState();
+}
+
+class _ConfirmEmailPageState extends State<ConfirmEmailPage> {
+  final _emailController = TextEditingController();
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    super.dispose();
+  }
+
+  Future passwordReset() async {
+    try {
+      await FirebaseAuth.instance
+          .sendPasswordResetEmail(email: _emailController.text.trim());
+
+      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+          backgroundColor: Colors.green, content: Text("Reset email sent")));
+
+      _emailController.clear();
+    } on FirebaseAuthException catch (e) {
+      print(e);
+
+      if (e.code == 'invalid-email') {
+        ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+          backgroundColor: Colors.red,
+          content: Text('Invalid email address.'),
+        ));
+      } else if (e.code == 'user-not-found') {
+        ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+          backgroundColor: Colors.red,
+          content: Text('No user found with this email.'),
+        ));
+      } else if (e.code == 'too-many-requests') {
+        ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+          backgroundColor: Colors.orange,
+          content: Text('Too many requests. Please try again later.'),
+        ));
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+          backgroundColor: Colors.red,
+          content: Text(e.message.toString()),
+        ));
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Form(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Image.asset("assets/images/logo2.png"),
+              const SizedBox(height: 25),
+              const Text(
+                "Enter email address",
+                style: TextStyle(fontSize: 16),
+              ),
+              const SizedBox(height: 25),
+              TextFormField(
+                controller: _emailController,
+                decoration: const InputDecoration(
+                  labelText: 'Email',
+                  prefixIcon: Icon(
+                    Icons.mail,
+                    color: Colors.grey,
+                    size: 18,
+                  ),
+                  border: OutlineInputBorder(),
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your email';
+                  }
+                  return null;
+                },
+              ),
+              CustomButton(
+                  text: "Send Code",
+                  onPressed: () {
+                    passwordReset();
+                  }),
+              const SizedBox(
+                height: 10,
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text('Create an account?'),
+                  const SizedBox(
+                    width: 5,
+                  ),
+                  InkWell(
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const SignUpPage()));
+                    },
+                    child: const Text(
+                      'Sign Up',
+                      style: TextStyle(
+                          color: primaryColor, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('Or'),
+                ],
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10),
+                    child: InkWell(
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const SignInPage()));
+                      },
+                      child: const Text(
+                        'Go back to Sign In',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: primaryColor,
+                            decoration: TextDecoration.underline,
+                            decorationThickness: 1),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/home.dart
@@ -0,0 +1,203 @@
+import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
+import 'package:kings_store/screens/category_screen.dart';
+import 'package:kings_store/widgets/cards/product_card.dart';
+import 'package:kings_store/widgets/carousel_banner.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    String name;
+
+    List categories = [
+      'Food',
+      'Fashion',
+      'Grocery',
+      'Electronics',
+    ];
+
+    List Categoryname = [
+      'Food',
+      'Fashion',
+      'Grocery',
+      'Electronics',
+    ];
+    return Scaffold(
+        backgroundColor: Colors.white,
+        body: SingleChildScrollView(
+          child: SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  // notification icon
+                  const Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [Icon(Iconsax.notification)],
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+
+                  // greetings
+                  const Text(
+                    "Welcome",
+                    style: TextStyle(fontWeight: FontWeight.w900),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+
+                  // search box and filter btn
+                  Row(
+                    children: [
+                      Container(
+                        width: MediaQuery.of(context).size.width / 1.5,
+                        padding: const EdgeInsets.all(13),
+                        decoration: BoxDecoration(
+                          color: Colors.grey.shade100,
+                          borderRadius: BorderRadius.circular(9),
+                        ),
+                        child: const Row(
+                          children: [
+                            Icon(
+                              Iconsax.search_normal,
+                              color: Colors.grey,
+                            ),
+                            SizedBox(width: 5),
+                            Text(
+                              "Search",
+                              style:
+                                  TextStyle(color: Colors.grey, fontSize: 16),
+                            )
+                          ],
+                        ),
+                      ),
+                      const Spacer(),
+                      Container(
+                        padding: const EdgeInsets.all(13),
+                        decoration: BoxDecoration(
+                          color: Colors.grey.shade100,
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: const Icon(
+                          Icons.tune,
+                          color: Colors.grey,
+                        ),
+                      )
+                    ],
+                  ),
+
+                  const SizedBox(
+                    height: 25,
+                  ),
+
+                  // slider carousel
+                  const CarouselBanner(),
+
+                  const SizedBox(
+                    height: 25,
+                  ),
+
+                  // categories section
+
+                  Row(
+                    children: [
+                      const Text(
+                        "Categories",
+                        style: TextStyle(
+                            fontSize: 16, fontWeight: FontWeight.bold),
+                      ),
+                      const Spacer(),
+                      Text("See all",
+                          style: TextStyle(
+                              fontSize: 16,
+                              color: Colors.blue[800],
+                              fontWeight: FontWeight.w500)),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  SizedBox(
+                    height: 35,
+                    child: ListView.builder(
+                        shrinkWrap: true,
+                        itemCount: categories.length,
+                        scrollDirection: Axis.horizontal,
+                        itemBuilder: (context, index) {
+                          return CategoryBtn(
+                            title: categories[index],
+                            name: Categoryname[index],
+                          );
+                        }),
+                  ),
+                  const SizedBox(
+                    height: 25,
+                  ),
+
+                  const Text(
+                    "Trending Products",
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(
+                    height: 15,
+                  ),
+
+                  const Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      ProductCards(),
+                      ProductCards(),
+                    ],
+                  ),
+
+                  const SizedBox(
+                    height: 20,
+                  ),
+
+                  const Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      ProductCards(),
+                      ProductCards(),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ));
+  }
+}
+
+class CategoryBtn extends StatelessWidget {
+  String title, name;
+  CategoryBtn({super.key, required this.title, required this.name});
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) => CategoryScreen(category: name)));
+      },
+      child: Container(
+        margin: const EdgeInsets.only(right: 20),
+        padding: const EdgeInsets.symmetric(vertical: 3, horizontal: 10),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(3),
+            color: Colors.grey.shade200),
+        child: Text(title,
+            style: TextStyle(
+                color: Colors.grey.shade600, fontWeight: FontWeight.w500)),
+      ),
+    );
+  }
+}
